0097dce1f7086758d6d4476fc176fe640d0b44e6
gabriel <ggarciaseco@gmail.com>
2018-05-07 22:08:08 +0200

    test: Add tests for the localStorage service

---
+++ client/src/services/LocalStorage.spec.js
@@ -0,0 +1,16 @@
+import { LocalStorageService } from './LocalStorage';
+
+describe('LocalStorage', () => {
+  const localStorage = new LocalStorageService();
+  it('should set an item in the localStorage and get it', () => {
+    const key = 'test';
+    const value = '1';
+    localStorage.set(key, value);
+    expect(localStorage.get(key)).toBe(value);
+  });
+
+  it('should get an empty string if the value is null', () => {
+    const key = 'nullValue';
+    expect(localStorage.get(key)).toBe('');
+  })
+});
